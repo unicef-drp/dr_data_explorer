a64af83f07eef0c065bf3cb99d57309719a3c1a9
Daniele <daoivot@tin.it>
2020-09-21 19:07:02 +0200

    Template page added

--- src/Controller/DataExplorerController.php
+++ src/Controller/DataExplorerController.php
@@ -20,11 +20,21 @@ class DataExplorerController extends ControllerBase
      */
     public function content()
     {
+        /*
         return [
             '#type' => 'markup',
             '#markup' => $this->t('Hello, World!'),
         ];
-    }
-}
+*/
 
-?>
+        $items=array(
+            array('name'=>'Article one'),
+            array('name'=>'Article two'),
+        );
+        return [
+            '#theme' => 'dr_data_explorer',
+            '#title' => "ttt3",
+            '#items'=>$items
+          ];
+    }
+} 
